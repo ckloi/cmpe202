7aa2e7404f7e251c3b1c318354851ca6f4aecb60
ckloi <ckloi@ucdavis.edu>
2019-04-12 23:31:25 -0700

    a class

--- lab9/functional/Client.java
+++ lab9/functional/Client.java
@@ -1,24 +1,68 @@
+import java.util.function.Function;
 
 public class Client {
     
     public void runTest()
     {
-        Handler h1 = new ConcreteHandler1() ;
-        Handler h2 = new ConcreteHandler2() ;
-        Handler h3 = new ConcreteHandler3() ;
+
+        Handler h1 = new Handler(){
+
+        private Handler successor = null;
+
+        public void handleRequest( String request, CustomResponse response ) {
+            System.out.println( "R1 got the request...");
+            if ( request.equalsIgnoreCase("R1") )
+            {
+                response.printResponse(this.getClass().getName() + " => This one is mine!");
+
+            }
+            else
+            {
+                if ( successor != null )
+                    successor.handleRequest(request, response);
+            }
+
+        }
+
+        public void setSuccessor(Handler next) {
+            this.successor = next ;
+        }
+
+    };
+
+        Handler h2 = new Handler(){
+
+        private Handler successor = null;
+
+        public void handleRequest( String request, CustomResponse response ) {
+            System.out.println( "R2 got the request...");
+            if ( request.equalsIgnoreCase("R1") )
+            {
+                response.printResponse(this.getClass().getName() + " => This one is mine!");
+
+            }
+            else
+            {
+                if ( successor != null )
+                    successor.handleRequest(request, response);
+            }
+
+        }
+
+        public void setSuccessor(Handler next) {
+            this.successor = next ;
+        }
+
+    };
 
         h1.setSuccessor(h2);
-        h2.setSuccessor(h3);
 
 
         System.out.println( "Sending R2...");
         h1.handleRequest("R2", (String x)-> {
             System.out.println(x);
         });
-        System.out.println( "Sending R3...");
-        h1.handleRequest("R3", (String x)-> {
-            System.out.println(x);
-        });
+
         System.out.println( "Sending R1...");
         h1.handleRequest("R1", (String x)-> {
             System.out.println(x);
